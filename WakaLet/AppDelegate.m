91b07a7e6732e0007f477267ee333022ae93835c
sdevore <sdevore@gmail.com>
2015-11-11 16:50:07 -0700

    format me style

--- WakaLet/AppDelegate.m
+++ WakaLet/AppDelegate.m
@@ -27,100 +27,108 @@
     [self checkAPIAndBuildMenu];
 }
 
--(void)checkAPIAndBuildMenu
-{
+- (void)checkAPIAndBuildMenu {
     // Get the manually set API key
     NSString *api_key = [[NSUserDefaults standardUserDefaults] valueForKey:@"api_key"];
-    
+
     // If there is no key though, just show the add-API-key entry
-    if( api_key == nil ){
-        [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Set API key" action:@selector(api) keyEquivalent:@"a"]];
+    if (api_key == nil || [api_key isEqualToString:@""]) {
+        [self addItemInMenuIfNeeded:
+                  [[NSMenuItem alloc] initWithTitle:@"Set API key" action:@selector(api) keyEquivalent:@"a"]];
     }
-    
+
     // If there is one, initiate the request
-    else{
+    else {
         // Get the current date, and 7 days back
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         [formatter setDateFormat:@"MM/dd/YYYY"];
         NSString *todayDate = [formatter stringFromDate:[NSDate date]];
-        NSString *someDaysAgoDate = [formatter stringFromDate:[[NSDate date] dateByAddingTimeInterval:3600*24*7*-1]];
-        
+        NSString *someDaysAgoDate =
+            [formatter stringFromDate:[[NSDate date] dateByAddingTimeInterval:3600 * 24 * 7 * -1]];
+
         // Build the parameters for the string
-        NSString *dataString = [NSString stringWithFormat:@"?start=%@&end=%@&api_key=%@",someDaysAgoDate,todayDate,api_key];
-        
+        NSString *dataString =
+            [NSString stringWithFormat:@"?start=%@&end=%@&api_key=%@", someDaysAgoDate, todayDate, api_key];
+
         // The general API url
-        NSString *urlString = [@"https://wakatime.com/api/v1/users/current/summaries" stringByAppendingString:dataString];
-        
+        NSString *urlString =
+            [@"https://wakatime.com/api/v1/users/current/summaries" stringByAppendingString:dataString];
+
         // Request the data
         NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
-        
+
         // Let the user know we are refreshing
         [statusMenu removeAllItems];
         [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Refreshing.." action:nil keyEquivalent:@""]];
-        
+
         // Record the date of the refresh/api get
-        [[NSUserDefaults standardUserDefaults] setValue:[NSDate date]
-                                                 forKey:@"refresh"];
-        
+        [[NSUserDefaults standardUserDefaults] setValue:[NSDate date] forKey:@"refresh"];
+
         // Go go go
-        [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-            
-            // Show possible errors
-            if( connectionError != nil ){
-                [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:connectionError.localizedDescription
-                                                               action:@selector(api)
-                                                        keyEquivalent:@""]];
-            }
-            
-            // Else just try to build the menu
-            else{
-                // The data
-                NSError *err;
-                NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&err];
-                
-                // If there is no error, build the wakalet
-                if( err == nil ) {
-                    [self performSelectorOnMainThread:@selector(buildMenuWithData:) withObject:dict waitUntilDone:NO];
-                }
-                
-                // Otherwise let's show the error somehow
-                else{
-                    [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:err.localizedDescription
-                                                                   action:@selector(api)
-                                                            keyEquivalent:@""]];
-                }
-            }
-        }];
+        [NSURLConnection
+            sendAsynchronousRequest:request
+                              queue:[[NSOperationQueue alloc] init]
+                  completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+
+                      // Show possible errors
+                      if (connectionError != nil) {
+                          [self addItemInMenuIfNeeded:[[NSMenuItem alloc]
+                                                          initWithTitle:connectionError.localizedDescription
+                                                                 action:@selector(api)
+                                                          keyEquivalent:@""]];
+                      }
+
+                      // Else just try to build the menu
+                      else {
+                          // The data
+                          NSError *err;
+                          NSDictionary *dict =
+                              [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&err];
+
+                          // If there is no error, build the wakalet
+                          if (err == nil) {
+                              [self performSelectorOnMainThread:@selector(buildMenuWithData:)
+                                                     withObject:dict
+                                                  waitUntilDone:NO];
+                          }
+
+                          // Otherwise let's show the error somehow
+                          else {
+                              [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:err.localizedDescription
+                                                                                     action:@selector(api)
+                                                                              keyEquivalent:@""]];
+                          }
+                      }
+                  }];
     }
 }
 
-- (void)addApiMenuWithTitle:(NSString *)title
-{
+- (void)addApiMenuWithTitle:(NSString *)title {
     dispatch_async(dispatch_get_main_queue(), ^{
         [statusMenu addItem:[[NSMenuItem alloc] initWithTitle:title action:@selector(api) keyEquivalent:@""]];
     });
 }
 
--(void)api
-{
+- (void)api {
     NSString *old_apo_key = [[NSUserDefaults standardUserDefaults] valueForKey:@"api_key"];
-    NSString *api_key = [self input:@"Enter your API key" defaultValue:old_apo_key==nil?@"":old_apo_key];
-    
-    if( api_key != nil ){
+    NSString *api_key = [self input:@"Enter your API key" defaultValue:old_apo_key == nil ? @"" : old_apo_key];
+
+    if (api_key != nil) {
         [[NSUserDefaults standardUserDefaults] setValue:api_key forKey:@"api_key"];
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
-    
+
     [self checkAPIAndBuildMenu];
 }
 
-- (NSString *)input: (NSString *)prompt defaultValue: (NSString *)defaultValue {
-    NSAlert *alert = [NSAlert alertWithMessageText:prompt
-                                     defaultButton:@"OK"
-                                   alternateButton:@"Cancel"
-                                       otherButton:nil
-                         informativeTextWithFormat:@"You can find your API key at https://wakatime.com/settings#apikey"];
-    
+- (NSString *)input:(NSString *)prompt defaultValue:(NSString *)defaultValue {
+    NSAlert *alert =
+        [NSAlert alertWithMessageText:prompt
+                        defaultButton:@"OK"
+                      alternateButton:@"Cancel"
+                          otherButton:nil
+            informativeTextWithFormat:@"You can find your API key at https://wakatime.com/settings#apikey"];
+
     NSTextField *input = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 400, 24)];
     [input setStringValue:defaultValue];
     [alert setAccessoryView:input];
@@ -137,30 +145,27 @@
     }
 }
 
--(void)menuWillOpen:(NSMenu *)menu
-{
-    NSMenuItem* timeSinceRefreshItem = [menu itemWithTag:1337];
+- (void)menuWillOpen:(NSMenu *)menu {
+    NSMenuItem *timeSinceRefreshItem = [menu itemWithTag:1337];
     timeSinceRefreshItem.title = [self getTimeSinceRefreshTitle];
 }
 
--(void)buildMenuWithData:(NSDictionary*)dict
-{
-    //NSLog(@"%@",dict);
-    
+- (void)buildMenuWithData:(NSDictionary *)dict {
+    // NSLog(@"%@",dict);
+
     // Remove any existing menu
     [statusMenu removeAllItems];
-    
+
     // This is a spacer object only
     NSMenuItem *spacer = [[NSMenuItem alloc] init];
     [spacer setView:[[NSView alloc] initWithFrame:NSMakeRect(0, 0, 1, 7)]];
     [statusMenu addItem:spacer];
-    
+
     // Let's find some data, reverse it to get it in the right order
     NSArray *dataObjArray = [[[dict valueForKey:@"data"] reverseObjectEnumerator] allObjects];
-    
+
     // Build stuff
     for (int i = 0; i < [dataObjArray count]; i++) {
-        
         // Get some data
         NSDictionary *dataObj = [dataObjArray objectAtIndex:i];
         NSDictionary *grandObj = [dataObj objectForKey:@"grand_total"];
@@ -169,22 +174,21 @@
         NSString *date = [rangeObj valueForKey:@"date"];
         NSString *date_human = [rangeObj valueForKey:@"text"];
         NSString *grand_total_text = [grandObj valueForKey:@"text"];
+
         // Each item is clickable, we store the url to the date in it's tooltip
-        NSMenuItem *dateItem = [[NSMenuItem alloc] initWithTitle:date_human.capitalizedString
-                                                          action:@selector(open:)
-                                                   keyEquivalent:@""];
-        dateItem.toolTip = [NSString stringWithFormat:@"http://www.wakatime.com/dashboard/day?date=%@",date];
+        NSMenuItem *dateItem =
+            [[NSMenuItem alloc] initWithTitle:date_human.capitalizedString action:@selector(open:) keyEquivalent:@""];
+        dateItem.toolTip = [NSString stringWithFormat:@"http://www.wakatime.com/dashboard/day?date=%@", date];
         [statusMenu addItem:dateItem];
-        
+
         // We only grab more data if there is data recorded
-        if( totalseconds > 0 ){
-            
+        if (totalseconds > 0) {
             // Get stuff from the projects object
             NSArray *projectsObjArray = [dataObj objectForKey:@"projects"];
             for (int j = 0; j < [projectsObjArray count]; j++) {
                 NSDictionary *projectObj = [projectsObjArray objectAtIndex:j];
-                NSString *name  = [projectObj valueForKey:@"name"];
-                NSString *time  = [projectObj valueForKey:@"digital"];
+                NSString *name = [projectObj valueForKey:@"name"];
+                NSString *time = [projectObj valueForKey:@"digital"];
                 name = [NSString stringWithFormat:@"  %@ (%@)", name, time];
                 NSMenuItem *item = [[NSMenuItem alloc] init];
                 item.enabled = NO;
@@ -193,45 +197,47 @@
                 [item setKeyEquivalent:@""];
                 [item setAction:nil];
                 [item setImage:nil];
-                item.toolTip = [NSString stringWithFormat:@"Total for day: %@",grand_total_text];
+                item.toolTip = [NSString stringWithFormat:@"Total for day: %@", grand_total_text];
                 [statusMenu addItem:item];
             }
         }
-        
+
         // Otherwise just show that there is no data for the date
-        else{
+        else {
             NSMenuItem *noDataItem = [[NSMenuItem alloc] initWithTitle:@"  No data" action:nil keyEquivalent:@""];
             noDataItem.enabled = NO;
             [statusMenu addItem:noDataItem];
         }
-        
+
         //
         NSMenuItem *spacer = [[NSMenuItem alloc] init];
         [spacer setView:[[NSView alloc] initWithFrame:NSMakeRect(0, 0, 1, 7)]];
         [statusMenu addItem:spacer];
     }
-    
+
     // Add verious other menu items
     [statusMenu addItem:[NSMenuItem separatorItem]];
-    
+
     // Calculate the last refresh time
-    NSMenuItem *timeSinceRefreshItem = [[NSMenuItem alloc] initWithTitle:[self getTimeSinceRefreshTitle]
-                                                                  action:nil keyEquivalent:@""];
+    NSMenuItem *timeSinceRefreshItem =
+        [[NSMenuItem alloc] initWithTitle:[self getTimeSinceRefreshTitle] action:nil keyEquivalent:@""];
     timeSinceRefreshItem.enabled = NO;
     timeSinceRefreshItem.tag = 1337;
     [statusMenu addItem:timeSinceRefreshItem];
-    
-    // Add more various stuffs
-    [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Refresh now" action:@selector(checkAPIAndBuildMenu) keyEquivalent:@"r"]];
-    [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Dashboard" action:@selector(dashboard) keyEquivalent:@"d"]];
-    [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Set API key" action:@selector(api) keyEquivalent:@"a"]];
 
+    // Add more various stuffs
+    [self addItemInMenuIfNeeded:[[NSMenuItem alloc] initWithTitle:@"Refresh now"
+                                                           action:@selector(checkAPIAndBuildMenu)
+                                                    keyEquivalent:@"r"]];
+    [self addItemInMenuIfNeeded:
+              [[NSMenuItem alloc] initWithTitle:@"Dashboard" action:@selector(dashboard) keyEquivalent:@"d"]];
+    [self addItemInMenuIfNeeded:
+              [[NSMenuItem alloc] initWithTitle:@"Set API key" action:@selector(api) keyEquivalent:@"a"]];
 }
 
-#pragma mark - 
+#pragma mark -
 
-- (BOOL)checkIsItemInMenu:(NSMenuItem *)checkItem
-{
+- (BOOL)checkIsItemInMenu:(NSMenuItem *)checkItem {
     for (NSMenuItem *item in statusMenu.itemArray) {
         if ([item isSameWithMenuItem:checkItem]) {
             return YES;
@@ -240,8 +246,7 @@
     return NO;
 }
 
-- (void)addItemInMenuIfNeeded:(NSMenuItem *)menuItem
-{
+- (void)addItemInMenuIfNeeded:(NSMenuItem *)menuItem {
     if (NO == [self checkIsItemInMenu:menuItem]) {
         [statusMenu addItem:menuItem];
     }
@@ -249,44 +254,38 @@
 
 #pragma mark Clicky methods
 
--(void)open:(NSMenuItem*)menuItem
-{
+- (void)open:(NSMenuItem *)menuItem {
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:menuItem.toolTip]];
 }
 
--(void)dashboard
-{
+- (void)dashboard {
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://www.wakatime.com/dashboard"]];
 }
 
--(NSString*)getTimeSinceRefreshTitle
-{
-    NSDate *refreshDate = (NSDate*)[[NSUserDefaults standardUserDefaults] valueForKey:@"refresh"];
+- (NSString *)getTimeSinceRefreshTitle {
+    NSDate *refreshDate = (NSDate *)[[NSUserDefaults standardUserDefaults] valueForKey:@"refresh"];
     int lastRefreshTimeStamp = (int)[refreshDate timeIntervalSince1970];
     int currentTimeStamp = (int)[[NSDate date] timeIntervalSince1970];
     int secondsAgo = currentTimeStamp - lastRefreshTimeStamp;
-    if( secondsAgo < 60 ){
-        if( secondsAgo == 1 ){
-            return [NSString stringWithFormat:@"Last refresh: %i second ago",secondsAgo];
-        }else{
-            return [NSString stringWithFormat:@"Last refresh: %i seconds ago",secondsAgo];
+    if (secondsAgo < 60) {
+        if (secondsAgo == 1) {
+            return [NSString stringWithFormat:@"Last refresh: %i second ago", secondsAgo];
+        } else {
+            return [NSString stringWithFormat:@"Last refresh: %i seconds ago", secondsAgo];
         }
-    }
-    else if( secondsAgo < 3600 ){
-        if( secondsAgo/60 == 1 ){
-            return [NSString stringWithFormat:@"Last refresh: Around %i minute ago",secondsAgo/60];
-        }else{
-            return [NSString stringWithFormat:@"Last refresh: Around %i minutes ago",secondsAgo/60];
+    } else if (secondsAgo < 3600) {
+        if (secondsAgo / 60 == 1) {
+            return [NSString stringWithFormat:@"Last refresh: Around %i minute ago", secondsAgo / 60];
+        } else {
+            return [NSString stringWithFormat:@"Last refresh: Around %i minutes ago", secondsAgo / 60];
         }
-    }
-    else if( secondsAgo < 3600*24){
-        if( secondsAgo/3600 == 1 ){
-            return [NSString stringWithFormat:@"Last refresh: Around %i hour ago",secondsAgo/3600];
-        }else{
-            return [NSString stringWithFormat:@"Last refresh: Around %i hours ago",secondsAgo/3600];
+    } else if (secondsAgo < 3600 * 24) {
+        if (secondsAgo / 3600 == 1) {
+            return [NSString stringWithFormat:@"Last refresh: Around %i hour ago", secondsAgo / 3600];
+        } else {
+            return [NSString stringWithFormat:@"Last refresh: Around %i hours ago", secondsAgo / 3600];
         }
-    }
-    else{
+    } else {
         return [NSString stringWithFormat:@"Last refresh: A looong time ago"];
     }
 }
